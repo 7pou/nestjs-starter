fbac536c19fb64dd513f605dbcef2bd770468608
7pou <604264588@qq.com>
2025-05-03 21:34:13 +0800

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeDatabase } from './config/database.config';
+import { initializeSwagger } from './config/swagger.config';
 async function bootstrap() {
   await initializeDatabase();
   const app = await NestFactory.create(AppModule);
+  initializeSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
