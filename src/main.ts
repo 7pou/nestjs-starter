d84aec7fa031dbaaa810630d0236543167507ad1
7pou <604264588@qq.com>
2025-05-03 17:09:14 +0800

    feat: import database

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { initializeDatabase } from './config/database.config';
 async function bootstrap() {
+  await initializeDatabase();
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
