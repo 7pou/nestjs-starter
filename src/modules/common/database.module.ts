f0cfe0ca8e70951409cbe04150b323300625870a
7pou <604264588@qq.com>
2025-05-03 16:58:16 +0800

    feat: add database module

---
+++ src/modules/common/database.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfig } from 'src/config/database.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => DatabaseConfig(),
+    }),
+  ],
+})
+export class DatabaseModule {}
